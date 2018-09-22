dde0ad9bb560490fb6bde1e472a659255751181b
Akanemiku <33932911+Akanemiku@users.noreply.github.com>
2018-09-22 22:32:51 +0800

    Create Resultlayer.java

---
+++ Horoscope/src/com/java/swing/horoscope/Resultlayer.java
@@ -0,0 +1,47 @@
+package com.java.swing.horoscope;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Resultlayer {
+
+	private JFrame frmResult;
+	public JFrame getFrmResult() {
+		return frmResult;
+	}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Resultlayer window = new Resultlayer();
+					window.frmResult.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Resultlayer() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmResult = new JFrame();
+		frmResult.setTitle("Result");
+		frmResult.setBounds(100, 100, 450, 300);
+		frmResult.setLocationRelativeTo(null);//窗体居中显示
+		frmResult.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+
+}
